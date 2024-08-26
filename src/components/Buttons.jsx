fbcfa20af4ff89c9ebb1f32b0f9dbe0f74f4fdd7
Turnerc7 <turnerc777@gmail.com>
2024-08-26 13:20:34 -0500

    Graded assignment #6

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,30 @@
+import './styling.css'
+import React from "react";
+import satData from "../components/satData.jsx";
+
+
+
+
+
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+ return (
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
+        return (
+          
+             <button onClick={() => filterByType(sat)} key={id} >
+               {sat} Orbit
+             </button>
+          
+        );
+      })}
+
+        <button onClick={() => setSat(satData)}>All Orbits</button>
+
     </div>
   );
-};
-
+}
+  
 export default Buttons;
